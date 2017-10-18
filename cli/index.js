6f881f16f468a908526cd50e326582fef09ff699
Oliver.W <ole3021@gmail.com>
2017-10-18 22:00:22 +0800

    add auth temp cache

--- cli/index.js
+++ cli/index.js
@@ -1,13 +1,52 @@
 #!/usr/bin/env node
 const version = require('../package.json').version
 const prog = require('caporal')
-const { getChangeInfo } = require('../lib')
+const inquirer = require('inquirer')
+const Preferences = require('preferences')
+
+const { initRepoInfo, initChangeInfo } = require('../lib')
+let prefs = new Preferences('moh-changes')
+
+const prepaerInfo = () => {
+  return initRepoInfo()
+}
+
+const login = () => new Promise((resolve, reject) => {
+  if (prefs.authInfo) return resolve(prefs.authInfo)
+
+  return inquirer.prompt([
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: value => value.length ? true : 'Please enter your username or e-mail address'
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: value => value.length ? true : 'Please enter your password'
+    }
+  ]).then(authInfo => {
+    prefs.authInfo = authInfo
+    resolve(authInfo)
+  })
+  .catch(reject)
+})
 
 prog
   .version(version)
   .description('A CLI tool to easily generate CHANGELOGS for project, based on PR on Github."')
   .action((args, options, logger) => {
-    getChangeInfo()
+    prepaerInfo()
+      .then(login)
+      .then(initChangeInfo)
+      .catch(err => {
+        if (err.code && err.code === 401) {
+          prefs.authInfo = undefined
+          console.log('>>> AuthInfo failed, please retry.')
+        }
+      })
   })
 
 prog.parse(process.argv)
---
+++ cli/login.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer')
+const Preferences = require('preferences')
+
+const prefs = new Preferences('moh-changes')
+
+exports.login = () => new Promise((resolve, reject) => {
+  inquirer.prompt([
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: value => value.length ? true : 'Please enter your username or e-mail address'
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: value => value.length ? true : 'Please enter your password'
+    }
+  ]).then(authInfo => {
+    return resolve(authInfo)
+  }).catch(err => {
+    return reject(err)
+  })
+})
